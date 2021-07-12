9ac6d9fb6cd812c5d10959c3b0585d4974c2d1e9
nickty <nickty.86@gmail.com>
2021-07-12 10:38:12 +0600

    single product component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import Product from './pages/Product';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path="/" exact>
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/product/:handle" exact>
+            <Product />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+    
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,29 @@
 import React, { useContext } from 'react'
 import { shopContext } from '../context/ShopProvider'
+import { Box, Grid, Text, Image } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
 
     const { products } = useContext(shopContext)
     return (
-        <div>
-           {JSON.stringify(products)}
-        </div>
+        <Box>
+            <Grid templateColumns="repeat(2, 1fr)">
+            {
+                products.map(product => (
+                    <Link to={`/product/${product.handle}`}>
+                        <Box _hover={{opacity: '80%'}}>
+                            <Image src={product.images[0].src} /> 
+                            <Text>{product.title}</Text> 
+                            <Text>${product.variants[0].price}</Text>
+                        </Box>
+                    </Link>
+                    
+                
+                ))
+            }
+            </Grid>
+        </Box>
     )
 }
 
