c16f282de0bc3669fa1f27f77ba4271f9c37a085
nickty <nickty.86@gmail.com>
2021-07-12 10:20:35 +0600

    showing navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Switch>
         <Route path="/" exact>
           <Home />
